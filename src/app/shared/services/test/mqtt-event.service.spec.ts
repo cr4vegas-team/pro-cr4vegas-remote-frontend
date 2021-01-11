f2be199711e2d03aca7550146d1f2d0fe62336b7
rubenfgr <rubenfgr@outlook.com>
2021-01-11 08:40:20 +0100

    update angular cli and core, and install mqtt package

--- src/app/shared/services/test/mqtt-event.service.spec.ts
+++ src/app/shared/services/test/mqtt-event.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MqttEventService } from './mqtt-event.service';
+import { MqttEventService } from '../mqtt-event.service';
 
 describe('MqttEventService', () => {
   let service: MqttEventService;
